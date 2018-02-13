10434eae473204c19360a79b0f702fea2be2b800
Hexxitfan111 <35806451+Hexxitfan111@users.noreply.github.com>
2018-02-13 09:12:35 -0600

    a

--- Assignments/school_page.js
+++ Assignments/school_page.js
@@ -6,3 +6,46 @@
 +--------------------------------------------------------------------------------------------+
 
 */
+
+sel_box_1 = document.getElementById("frame_1_select"); //Variable for easier later assignmentof data to box 1
+sel_box_2 = document.getElementById("frame_2_select"); //Variable for easier later assignmentof data to box 2
+start_box_1 = 1; //Starting value for box 1
+start_box_2 = 2; //Starting value for box 2
+data_hog = XML
+data_win =
+data_dur =
+data_arc =
+data_cas =
+
+
+function switch_box(box_number) {
+	// 'a' should be equal to 1, 2, 3, 4, or 5.
+	switch (box_number) {
+		case 1:
+			switch (sel_box_1.value) {
+				case 1:
+				
+					return;
+				case 2:
+					return;
+				case 3:
+					return;
+				case 4:
+					return;
+				case 5:
+					return;
+			}
+		case 2:
+		  switch (sel_box_2.value) {
+				case 1:
+					return;
+				case 2:
+					return;
+				case 3:
+					return;
+				case 4:
+					return;
+				case 5:
+					return;
+	}
+}
