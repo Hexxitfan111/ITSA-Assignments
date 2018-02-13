adfd98bb5db6134c2938b3706a78661115176893
Hexxitfan111 <35806451+Hexxitfan111@users.noreply.github.com>
2018-02-13 09:38:00 -0600

    attempts to import files into js

--- Assignments/school_page.js
+++ Assignments/school_page.js
@@ -6,26 +6,41 @@
 +--------------------------------------------------------------------------------------------+
 
 */
-
 sel_box_1 = document.getElementById("frame_1_select"); //Variable for easier later assignmentof data to box 1
 sel_box_2 = document.getElementById("frame_2_select"); //Variable for easier later assignmentof data to box 2
 start_box_1 = 1; //Starting value for box 1
 start_box_2 = 2; //Starting value for box 2
-data_hog = var client = new XMLHttpRequest();
 
-client.open('GET', '/hogwarts_data.txt');
-client.onreadystatechange = function() {
-  alert(client.responseText);
+
+//---------------------------------------------------
+data_hog = new XMLHttpRequest();
+data_hog.open("GET", "hogwarts_data.html", true);
+data_hog.onreadystatechange = function() {
+  if (data_hog.readyState === 4) {  // Makes sure the document is ready to parse.
+    if (data_hog.status === 200) {  // Makes sure it's found the file.
+      allText = data_hog.responseText; 
+      //lines = data_hog.responseText.split("\n"); // Will separate each line into an array
+      var customTextElement = document.getElementById('textHolder');
+customTextElement.innerHTML = data_hog.responseText;
+    }
+  }
 }
-client.send();
-
-data_win = var client = new XMLHttpRequest();
-client.open('GET', '/winterhold_data.txt');
-client.onreadystatechange = function() {
-  alert(client.responseText);
+data_hog.send(null);
+//------------------------------------------------------
+data_win = new XMLHttpRequest();
+data_win.open("GET", "winterhold_data.html", true);
+data_win.onreadystatechange = function() {
+  if (data_win.readyState === 4) {  // Makes sure the document is ready to parse.
+    if (data_win.status === 200) {  // Makes sure it's found the file.
+      allText = data_win.responseText; 
+      //lines = data_win.responseText.split("\n"); // Will separate each line into an array
+      var customTextElement = document.getElementById('textHolder');
+customTextElement.innerHTML = data_win.responseText;
+    }
+  }
 }
-client.send();
-
+data_win.send(null);
+//--------------------------------------------------------
 data_dur =
 data_arc =
 data_cas =
@@ -62,4 +77,5 @@ function switch_box(box_number) {
 				case 5:
 					return;
 	}
+}
 }
