610ca49c2528d7b3453033e27d3e1bed5408b583
Hexxitfan111 <35806451+Hexxitfan111@users.noreply.github.com>
2018-02-14 09:28:47 -0600

    have given up on text files

--- Assignments/school_page.js
+++ Assignments/school_page.js
@@ -13,56 +13,70 @@ start_box_2 = 2; //Starting value for box 2
 
 //---------------------------------------------------
 
-data_hog = new XMLHttpRequest();
-data_hog.open('GET', 'https://cors-anywhere.herokuapp.com/http://github.com/Hexxitfan111/ITSA-Assignments/blob/master/Assignments/hogwarts_data.html');
-data_hog.onload = function() {
-    if (data_hog.status === 200) {
-        alert(data_hog.responseText);
-    }
-    else {
-        alert('Request failed.  Returned status of ' + data_hog.status);
-    }
-};
-data_hog.send();
-data_dur = "";
-data_arc = "";
-data_cas = "";
+import 'school_page_data'
+
+function init() {
+init_1 = document.getElementById("frame_main_1")
+init_2 = document.getElementById("frame_main_2")
+init_2.innerHTML = school_page_data.data_hog;
+init_1.innerHTML = school_page_data.data_win;
+}
 
 
-function switch_box(box_number) {
+function switch_box_1() {
 	// 'a' should be equal to 1, 2, 3, 4, or 5.
-	sel_box_1 = document.getElementById("frame_1_select"); //Variable for easier later assignmentof data to box 1
-	sel_box_2 = document.getElementById("frame_2_select"); //Variable for easier later assignmentof data to box 2
+	//sel_box_1 = document.getElementById("frame_1_select"); //Variable for easier later assignment of data to box 1
+	//sel_box_2 = document.getElementById("frame_2_select"); //Variable for easier later assignment of data to box 2
 	frame_main_1 = document.getElementById("frame_main_1");
 	frame_main_2 = document.getElementById("frame_main_2");
-	switch (box_number) {
-		case 1:
-			switch (sel_box_1.value) {
-				case 1:
-					frame_main_1.innerHTML = load_dat(1);
-					return;
-				case 2:
-					frame_main_1.innerHTML = load_dat(2);
-					return;
-				case 3:
-					return;
-				case 4:
-					return;
-				case 5:
-					return;
-			}
-		case 2:
-		  switch (sel_box_2.value) {
-				case 1:
-					return;
-				case 2:
-					return;
-				case 3:
-					return;
-				case 4:
-					return;
-				case 5:
-					return;
+	//frame_main_1.innerHTML = data_hog;
+	alert('box 1')
+	switch (document.getElementById("frame_1_select").value) {
+		case '1':
+			alert('1')
+			frame_main_1.innerHTML = data_win;
+			return;
+		case '2':
+			alert('2')
+			frame_main_1.innerHTML = data_hog;
+			return;
+		case '3':
+			alert('3');
+			return;
+		case '4':
+			alert('4');
+			return;
+		case '5':
+		alert('5')
+			return;
 	}
 }
+function switch_box_2() {
+	// 'a' should be equal to 1, 2, 3, 4, or 5.
+	//sel_box_1 = document.getElementById("frame_1_select"); //Variable for easier later assignment of data to box 1
+	//sel_box_2 = document.getElementById("frame_2_select"); //Variable for easier later assignment of data to box 2
+	frame_main_1 = document.getElementById("frame_main_1");
+	frame_main_2 = document.getElementById("frame_main_2");
+	//frame_main_1.innerHTML = data_hog;
+	alert('box 2')
+	switch (document.getElementById("frame_2_select").value) {
+		case '1':
+			alert('1')
+			frame_main_2.innerHTML = data_win;
+			return;
+		case '2':
+			alert('2')
+			frame_main_2.innerHTML = data_hog;
+			return;
+		case '3':
+			alert('3');
+			return;
+		case '4':
+			alert('4');
+			return;
+		case '5':
+			alert('5')
+			return;
+	}
 }
+
